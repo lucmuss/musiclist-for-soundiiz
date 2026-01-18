4ab99f6dcc2b75f65d6ef58931d68f81cb4432c0
Lucas M <lucas.mussmaecher@gmail.com>
2026-01-18 20:36:15 +0100

    Reduce input directory listbox height from 4 to 3 lines

--- src/musiclist_for_soundiiz/gui.py
+++ src/musiclist_for_soundiiz/gui.py
@@ -122,7 +122,7 @@ class MusicListGUI:
             list_frame,
             yscrollcommand=scrollbar.set,
             selectmode=tk.EXTENDED,
-            height=4
+            height=3
         )
         self.dir_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         scrollbar.config(command=self.dir_listbox.yview)
